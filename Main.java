bc400e550d127136046e129fc001ce57b7ed7a23
Jesus Salvador Cortes Gutierrez <31747651+noxtla@users.noreply.github.com>
2019-02-12 16:49:16 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,19 @@
+package Condicionales;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+	int numero,i=0;
+	
+	numero = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el numero "));
+
+	 while (i<=numero) {
+		 JOptionPane.showMessageDialog(null, "El numero es " + i);
+		 i++;
+	 }
+		 	
+	}
+}
 
